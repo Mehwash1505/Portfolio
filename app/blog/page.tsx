01f5aa3347a7abb6057abe3c766c797ee6528217
Mehwash Nasir <asmanasreen388@gmail.com>
2026-01-19 19:32:28 +0530

    BlogPage export

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+import { getAllPosts } from "../../lib/blog" 
+import Container from "../../components/ui/Container"
+import BlogList from "./BlogList"
+
+export default function BlogPage() {
+  const posts = getAllPosts()
+
+  return (
+    <Container>
+      <h1 className="text-3xl font-semibold">Blog</h1>
+      <BlogList posts={posts} />
+    </Container>
+  )
+}
