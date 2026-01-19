6a54cc0bb87c884133dc09f7605e602ef8342374
Mehwash Nasir <asmanasreen388@gmail.com>
2026-01-19 20:06:58 +0530

    blog page

---
+++ lib/blog.ts
@@ -0,0 +1,25 @@
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+
+const BLOG_PATH = path.join(process.cwd(), "src/content/blog")
+
+export function getAllPosts() {
+  const files = fs.readdirSync(BLOG_PATH)
+
+  return files.map(file => {
+    const slug = file.replace(".mdx", "")
+    const fileContent = fs.readFileSync(
+      path.join(BLOG_PATH, file),
+      "utf-8"
+    )
+
+    const { data } = matter(fileContent)
+
+    return {
+      slug,
+      title: data.title,
+      date: data.date
+    }
+  })
+}
