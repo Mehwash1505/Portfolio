49c0625d543da66344e161e1366d0e8e9ef91a6b
Mehwash Nasir <asmanasreen388@gmail.com>
2026-01-19 20:01:16 +0530

    navbar

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav className="px-6 py-4 border-b border-neutral-800">
+      <div className="max-w-5xl mx-auto flex justify-between">
+        <Link href="/" className="font-medium text-white">
+          Mehwash
+        </Link>
+
+        <Link
+          href="/projects"
+          className="text-neutral-400 hover:text-white transition"
+        >
+          Projects
+        </Link>
+      </div>
+    </nav>
+  )
+}
